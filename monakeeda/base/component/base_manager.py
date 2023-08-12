9effbd9ba139751becf007ccc58b5639f1a49599
liel bondy <lielbondy@gmail.com>
2023-08-12 22:49:54 +0300

    [+] Added base manager class that incapsulates bases management logic

---
+++ monakeeda/base/component/base_manager.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+
+class BaseManager(ABC):
+    @abstractmethod
+    def _set_by_base(self, monkey_cls, base, monkey_attrs):
+        pass
+
+    @abstractmethod
+    def _set_curr_cls(self, monkey_cls, bases, monkey_attrs):
+        pass
+
+    def run_bases(self, monkey_cls, bases, monkey_attrs):
+        # TODO: add operation order
+
+        for base in bases:
+            self._set_by_base(monkey_cls, base, monkey_attrs)
+
+        self._set_curr_cls(monkey_cls, bases, monkey_attrs)
