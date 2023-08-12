ed3089d109da16701a336595e06a9a3b7cfbfab5
liel bondy <lielbondy@gmail.com>
2023-08-12 20:09:50 +0300

    [+] Added Parameter Component with simple common half implementation

---
+++ monakeeda/base/component/parameter_component.py
@@ -0,0 +1,37 @@
+from abc import abstractmethod, ABC
+from typing import ClassVar, Union, TypeVar, Type
+
+from .component import Component
+from .rules import RuleException, Rule
+
+
+class Parameter(Component):
+    __key__: ClassVar[str]
+
+    def __init__(self, param_val):
+        self.param_val = param_val
+
+    def __str__(self):
+        return self.__key__
+
+
+TParameter = TypeVar('TParameter', bound=Type[Parameter])
+
+
+class ParameterValueTypeValidationFailedRuleException(RuleException):
+    def __init__(self, parameter_key, value, wanted_type):
+        self.parameter_key = parameter_key
+        self.value = value
+        self.wanted_type = wanted_type
+
+    def __str__(self):
+        return f"{self.parameter_key} -> value {self.value} not of type {self.wanted_type}"
+
+
+class BaseParameterValueTypeValidationFailedRule(Rule, ABC):
+    def __init__(self, wanted_type):
+        self.wanted_type = wanted_type
+
+    @abstractmethod
+    def validate(self, component: Parameter) -> Union[RuleException, None]:
+        pass
