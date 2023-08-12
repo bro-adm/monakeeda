b5fae4de4d4df291ea524bf93a674085f3724fd5
liel bondy <lielbondy@gmail.com>
2023-08-12 16:12:43 +0300

    [+] Added Component ABC class

---
+++ monakeeda/base/component/component.py
@@ -0,0 +1,73 @@
+from abc import ABC, abstractmethod
+from typing import ClassVar, TypeVar, Type
+
+from .rules import Rules
+
+
+# TODO: add success type of type bool for type hints
+
+class Component(ABC):
+    __rules__: ClassVar[Rules] = Rules([])
+
+    def _validate(self, monkey_cls, bases, monkey_attrs) -> bool:
+        # TODO: try to use self.__class__.__rules__ for logic consistency
+        """
+        Pipeline ? -> step X (?)
+
+        :return: True if valid and False if Errors occurred
+        """
+
+        exceptions = self.__rules__.validate(self)
+        monkey_attrs['build']['exceptions'] = exceptions
+
+        if exceptions:
+            return False
+
+        return True
+
+    @abstractmethod
+    def _set_cls_landscape(self, monkey_cls, bases, monkey_attrs):
+        """
+        Pipeline ModelCls Build -> step X (?)
+
+        :param monkey_cls: the MonkeyModel class itself
+        :param bases: all the classes the MonkeyModel inherited from
+        :param monkey_attrs: all the attributed the MonkeyModel holds
+
+        :return: updated MonkeyModel attributes
+        """
+
+        pass
+
+    def build(self, monkey_cls, bases, monkey_attrs) -> bool:
+        is_valid = self._validate(monkey_cls, bases, monkey_attrs)
+
+        if not is_valid:
+            return is_valid
+
+        self._set_cls_landscape(monkey_cls, bases, monkey_attrs)
+
+        return True
+
+    @abstractmethod
+    def values_handler(self, key, model_instance, values) -> dict:
+        # TODO: add key param description
+        # TODO: type validations happen at this stage so how come they cant return RulesException
+
+        """
+        Pipeline step X (?)
+
+        Some Component implementations do not need and cannot and do not see the full values dictionary given to Model
+        on init, so merging the values to the main values dictionary is up to the Manager Component implementation.
+
+        :param key: ?
+        :param model_instance: MonkeyModel -> do all components receive the instance?
+        :param values: values to initiate component
+
+        :return: updated values
+        """
+
+        pass
+
+
+TComponent = TypeVar('TComponent', bound=Type[Component])
