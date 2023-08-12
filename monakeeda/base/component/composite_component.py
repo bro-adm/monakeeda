ebf03da5552d4abb9a96e3c6a637063290c4b491
liel bondy <lielbondy@gmail.com>
2023-08-12 16:13:18 +0300

    [+] Added composite components base and basic composite by init implementation

---
+++ monakeeda/base/component/composite_component.py
@@ -0,0 +1,49 @@
+from abc import ABC, abstractmethod
+from typing import List, Generic
+
+from .component import TComponent, Component
+
+
+class BaseComponentComposite(Component, Generic[TComponent], ABC):
+    """
+    Composite DP implementation.
+
+    The components list is not available always via init.
+    So this is an ABC implementation with all additional components listing discovery methods
+    being custom one level inheritance implementations.
+    """
+
+    @property
+    @abstractmethod
+    def _components(self) -> List[TComponent]:
+        pass
+
+    def values_handler(self, key, model_instance, values) -> dict:
+        calculated_values = values
+        for component in self._components:
+            calculated_values = component.values_handler(key, model_instance, calculated_values)
+
+        return calculated_values
+
+    def _set_cls_landscape(self, monkey_cls, bases, monkey_attrs):
+        # TODO: decide if to make this method not abstract with this nothing implementation
+
+        pass
+
+    def build(self, monkey_cls, bases, monkey_attrs) -> bool:
+        is_valid = True
+
+        for component in self._components:
+            # Keeps is_valid False if was False, else runs next components build (for additional errors and setups)
+            is_valid = is_valid if not is_valid else component.build(monkey_cls, bases, monkey_attrs)
+
+        return is_valid
+
+
+class ComponentInitComposite(BaseComponentComposite, Generic[TComponent]):
+    def __init__(self, components: List[TComponent]):
+        self._init_components = components
+
+    @property
+    def _components(self) -> List[TComponent]:
+        return self._init_components
