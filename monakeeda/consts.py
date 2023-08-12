805ae908a7147b83c2a93584ae22a551f24ed15d
liel bondy <lielbondy@gmail.com>
2023-08-13 00:03:11 +0300

    [+] Added decorated_with and decorators namespaces in consts class

--- monakeeda/consts.py
+++ monakeeda/consts.py
@@ -1,3 +1,6 @@
 class NamespacesConsts:
     BUILD = 'build'
     EXCEPTIONS = 'exceptions'
+    DECORATORS = 'decorators'
+
+    DECORATED_WITH = '__decorated_with__'  # model methods decorators instances space
