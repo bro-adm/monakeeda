0039807bda7fa0cdc2c6c362d6ae76843c023398
liel bondy <lielbondy@gmail.com>
2023-08-13 00:07:19 +0300

    [+] Made main component run bases setup by default

--- monakeeda/base/component/main_component.py
+++ monakeeda/base/component/main_component.py
@@ -7,4 +7,6 @@ from .composite_component import BaseComponentComposite
 
 
 class MainComponent(BaseComponentComposite[TComponent], BaseManager, Generic[TComponent], ABC):
-    pass
+    def _set_cls_landscape(self, monkey_cls, bases, monkey_attrs):
+        super()._set_cls_landscape(monkey_cls, bases, monkey_attrs)
+        self.run_bases(monkey_cls, bases, monkey_attrs)
