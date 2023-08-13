d3bf638a9c3534d459cd961b107cf2a050179cb4
liel bondy <lielbondy@gmail.com>
2023-08-13 13:35:15 +0300

    [+] Added fields namespaces and keywords along with python native ones as consts

--- monakeeda/consts.py
+++ monakeeda/consts.py
@@ -1,6 +1,17 @@
+class PythonNamingConsts:
+    annotations = '__annotations__'
+
+
 class NamespacesConsts:
     BUILD = 'build'
     EXCEPTIONS = 'exceptions'
     DECORATORS = 'decorators'
+    FIELDS = 'fields_info'  # TODO: validate if can change to 'fields'
+    FIELDS_KEYS = 'main_field_keys'  # TODO: validate if can get rid of this
 
     DECORATED_WITH = '__decorated_with__'  # model methods decorators instances space
+
+
+class FieldConsts:
+    FIELD = 'field'
+    REQUIRED = 'required'
