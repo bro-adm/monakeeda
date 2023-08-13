0c1e47030000d4342beaae5ed78e73feb94730be
liel bondy <lielbondy@gmail.com>
2023-08-13 16:06:14 +0300

    [+] Added Main COmponent doc str

--- monakeeda/base/component/main_component.py
+++ monakeeda/base/component/main_component.py
@@ -7,6 +7,11 @@ from .composite_component import BaseComponentComposite
 
 
 class MainComponent(BaseComponentComposite[TComponent], BaseManager, Generic[TComponent], ABC):
+    """
+    Meant for concept managers that usually if not always need to also run bases setups.
+    Beware of instance variables!!!
+    """
+
     def _set_cls_landscape(self, monkey_cls, bases, monkey_attrs):
         super()._set_cls_landscape(monkey_cls, bases, monkey_attrs)
         self.run_bases(monkey_cls, bases, monkey_attrs)
