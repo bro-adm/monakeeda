1f1c8556c0e20d9b5a73c2b8d17b806a385b6d38
liel bondy <lielbondy@gmail.com>
2023-08-13 15:14:11 +0300

    [+] Added annotations init imports

---
+++ monakeeda/base/annotations/__init__.py
@@ -0,0 +1,5 @@
+from .annotations import ModelAnnotation
+from .base_annotation import Annotation, GenericAnnotation
+from .component import AnnotationMainComponent
+from .helpers import get_type_cls
+from .mapping import annotation_mapper, annotation_mapping
