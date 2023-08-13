af89f43949a47e47bd0811ef013d7f06235b00f4
liel bondy <lielbondy@gmail.com>
2023-08-14 00:04:58 +0300

    [] Patched for now the logic of FieldComponen, FieldParamater _set_cls_landscape and accordingly their inits and methods

--- monakeeda/base/fields/base_fields.py
+++ monakeeda/base/fields/base_fields.py
@@ -1,21 +1,36 @@
 from abc import ABC
 
-from monakeeda.consts import FieldConsts
+from monakeeda.consts import FieldConsts, NamespacesConsts
 from ..component import ConfigurableComponent, Parameter
 
 
 class FieldParameter(Parameter, ABC):
 
+    def __init__(self, field_key: str, param_val):
+        self._field_key = field_key
+        super().__init__(param_val)
+
     def _set_cls_landscape(self, monkey_cls, bases, monkey_attrs):
         super()._set_cls_landscape(monkey_cls, bases, monkey_attrs)
-        monkey_attrs[self.__key__] = self.param_val
+        monkey_attrs[NamespacesConsts.FIELDS][self._field_key][self.__key__] = self.param_val
 
 
 class Field(ConfigurableComponent[FieldParameter]):
 
+    def __init__(self, field_key: str, **params):
+        self._field_key = field_key
+        super().__init__(**params)
+
+    def __initiate_params(self, params: dict):
+        for param_key, param_val in params.items():
+            for possible_param in self.__parameters_components__:
+                if param_key == possible_param.__key__:
+                    self._initialized_params.append(possible_param(self._field_key, param_val))
+                    break
+
     def _set_cls_landscape(self, monkey_cls, bases, monkey_attrs):
         super(Field, self)._set_cls_landscape(monkey_cls, bases, monkey_attrs)
-        monkey_attrs[FieldConsts.FIELD] = self
+        monkey_attrs[NamespacesConsts.FIELDS][self._field_key][FieldConsts.FIELD] = self
 
 
 class NoField(Field, copy_parameter_components=False):
