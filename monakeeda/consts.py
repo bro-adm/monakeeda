979501b6bc81dde3cdfcc5b1128fc10d3400211d
liel bondy <lielbondy@gmail.com>
2023-08-13 15:14:42 +0300

    [+] Added more field consts and another namespace const

--- monakeeda/consts.py
+++ monakeeda/consts.py
@@ -8,6 +8,7 @@ class NamespacesConsts:
     DECORATORS = 'decorators'
     FIELDS = 'fields_info'  # TODO: validate if can change to 'fields'
     FIELDS_KEYS = 'main_field_keys'  # TODO: validate if can get rid of this
+    ANNOTATIONS = 'annotations'
 
     DECORATED_WITH = '__decorated_with__'  # model methods decorators instances space
 
@@ -15,3 +16,5 @@ class NamespacesConsts:
 class FieldConsts:
     FIELD = 'field'
     REQUIRED = 'required'
+    TYPE = 'type'
+    ANNOTATION = 'annotation_cls'
