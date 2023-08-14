8b16d124f7c1dbcbf318a997c3a17d3a3cc7f15e
liel bondy <lielbondy@gmail.com>
2023-08-14 15:34:36 +0300

    [+] Added a cast annotation

---
+++ monakeeda/implementations/annotations/nice_to_have/cast.py
@@ -0,0 +1,21 @@
+from typing import Generic, T
+
+from monakeeda.base import GenericAnnotation
+from monakeeda.consts import FieldConsts
+
+
+class CastingError(ValueError):
+    def __init__(self, given_val, wanted_casting_type):
+        super(CastingError, self).__init__(
+            f'failed casting from type {given_val} to {wanted_casting_type} -> value given is {given_val}')
+
+
+class Cast(GenericAnnotation, Generic[T]):
+    def _act_with_value(self, value, cls, current_field_info):
+        cast_to = self._types[0]
+        try:
+            wanted_value = cast_to(value)
+            current_field_info[FieldConsts.TYPE] = cast_to
+            return wanted_value
+        except TypeError:
+            raise CastingError(value, cast_to)
