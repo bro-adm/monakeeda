e12d8e2dedc0ba6378c123de2c51b91dfa41be11
liel bondy <lielbondy@gmail.com>
2023-08-15 01:58:39 +0300

    [] Patched update logic along with new __setattr__ logic

--- monakeeda/base/model/base_model.py
+++ monakeeda/base/model/base_model.py
@@ -4,6 +4,7 @@ from ..config import ConfigMainComponent, Config
 from ..decorators import DecoratorMainComponent
 from ..fields import FieldMainComponent
 from ..meta import MonkeyMeta
+from ...utils import deep_update
 
 # TODO: create signature component
 model_components = MainComponentInitComposite([FieldMainComponent(), DecoratorMainComponent(), AnnotationMainComponent(annotation_mapping), ConfigMainComponent()])
@@ -21,12 +22,18 @@ class MonkeyModel(metaclass=MonkeyMeta, model_components=model_components, annot
         # The super setter because the setter logic can be changed and in teh init we have data as we want it already
 
     def __init__(self, **kwargs):
+        super().__setattr__('_values', kwargs.copy())
         self._values_handler(kwargs, Stages.INIT)
         # The super setter because the setter logic can be changed and in teh init we have data as we want it already
 
     # TODO: patch update
     def update(self, **kwargs):
+        kwargs = deep_update(self._values, kwargs)
+        super().__setattr__('_values', kwargs.copy())
         self._values_handler(kwargs, Stages.UPDATE)
+
+    def __setattr__(self, key, value):
+        self.update(**{key: value})
 
     def __init_subclass__(cls):
         # can't put the start of __map__ here because the set_cls_namespace happens before in the meta cls
