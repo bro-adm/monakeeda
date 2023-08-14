a27357c9a31743efcfbb52fc2a18ba8e48098f55
liel bondy <lielbondy@gmail.com>
2023-08-14 15:26:43 +0300

    [+] Added base dir init imports

---
+++ monakeeda/base/__init__.py
@@ -0,0 +1,7 @@
+from .annotations import Annotation, GenericAnnotation, annotation_mapper
+from .component import *
+from .config import *
+from .decorators import BaseDecorator
+from .fields import *
+from .meta import MonkeyMeta
+from .model import MonkeyModel
