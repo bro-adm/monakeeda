dd32cc967b67b07fe2b490c72c3655e14336b314
liel bondy <lielbondy@gmail.com>
2023-08-14 17:29:08 +0300

    [+] Added update method to model

--- monakeeda/base/model/base_model.py
+++ monakeeda/base/model/base_model.py
@@ -1,10 +1,11 @@
 from .helpers import ignore_unwanted_params
 from ..annotations import AnnotationMainComponent, annotation_mapping, ModelAnnotation
-from ..component import ComponentInitComposite
+from ..component import ComponentInitComposite, Stages
 from ..config import ConfigMainComponent, Config
 from ..decorators import DecoratorMainComponent
 from ..fields import FieldMainComponent
 from ..meta import MonkeyMeta
+from ...consts import NamespacesConsts
 
 # TODO: create signature component
 model_components = \
@@ -19,13 +20,19 @@ model_components = \
 # TODO: add update method and dict/view method
 class MonkeyModel(metaclass=MonkeyMeta, model_components=model_components, annotation_mapping=annotation_mapping):
 
-    def __init__(self, **kwargs):
-        for key in self.__map__['main_field_keys']:
-            kwargs = self.__model_components__.values_handler(key, self, kwargs)
+    def _values_handler(self, values: dict, stage):
+        for key in self.__map__[NamespacesConsts.FIELDS_KEYS]:
+            kwargs = self.__model_components__.values_handler(key, self, kwargs, stage)
         kwargs = ignore_unwanted_params(self.__class__, kwargs)
         for key in kwargs:
             super(MonkeyModel, self).__setattr__(key, kwargs[key])
         # The super setter because the setter logic can be changed and in teh init we have data as we want it already
+    def __init__(self, **kwargs):
+        self._values_handler(kwargs, Stages.INIT)
+        # The super setter because the setter logic can be changed and in teh init we have data as we want it already
+
+    def update(self, **kwargs):
+        self._values_handler(kwargs, Stages.UPDATE)
 
     def __init_subclass__(cls):
         # can't put the start of __map__ here because the set_cls_namespace happens before in the meta cls
