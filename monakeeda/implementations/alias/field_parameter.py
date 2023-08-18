5ff5add97acd3b5b2e03598a3092d811b1bedd83
liel bondy <lielbondy@gmail.com>
2023-08-19 02:45:17 +0300

    [+] Added field parameter alias implementation

---
+++ monakeeda/implementations/alias/field_parameter.py
@@ -0,0 +1,22 @@
+import inspect
+
+from monakeeda.base import FieldParameter, Rules, Field
+from ..rules import BasicParameterValueTypeValidationRule
+
+
+@Field.parameter
+class Alias(FieldParameter):
+    __key__ = 'alias'
+    __label__ = 'alias'
+    __rules__ = Rules([BasicParameterValueTypeValidationRule(str)])
+
+    def values_handler(self, model_instance, values, stage) -> dict:
+        updated_values = {}
+
+        values.setdefault(self.param_val, inspect._empty)
+        field_val_by_alias = values.pop(self.param_val)
+
+        if field_val_by_alias != inspect._empty:
+            updated_values[self._field_key] = field_val_by_alias
+
+        return updated_values
