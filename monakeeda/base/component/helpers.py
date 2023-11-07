ac57b1883dca2da052f8cc5b46983af41aa9acbe
liel bondy <lielbondy@gmail.com>
2023-11-07 22:10:41 +0200

    [-] Reoved organization helper from components will be added in later commit to different location

--- monakeeda/base/component/helpers.py
+++ monakeeda/base/component/helpers.py
@@ -1,24 +1,7 @@
-from collections import OrderedDict
-from typing import List, Type, Dict
+from typing import Type
 
-from .component import Component, all_components
 from .configurable_component import ConfigurableComponent
 from .parameter_component import Parameter
-
-
-def organize_components(monkey_components: List[Component]) -> Dict[Type[Component], List[Component]]:
-    ordered_dict = OrderedDict()
-
-    for component_type in all_components:
-        tmp = []
-
-        for component in monkey_components:
-            if str(type(component)) == component_type:
-                tmp.append(component)
-
-        ordered_dict[component_type] = tmp
-
-    return ordered_dict
 
 
 def get_parameter_component_by_label(configurable_component: Type[ConfigurableComponent], label: str) -> Type[
