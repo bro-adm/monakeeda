3a6c4ab3ac84a9e9d972348940de165ed8cf1b5a
liel bondy <lielbondy@gmail.com>
2023-11-07 22:21:50 +0200

    [] Patched helper get_cls_attrs used in config setter to get all the class attr to initalize it with -> issue was with inheritance

--- monakeeda/helpers.py
+++ monakeeda/helpers.py
@@ -21,5 +21,12 @@ def get_param_default_from_signature(cls, key: str):
 def get_cls_attrs(cls) -> dict:
     # returns only custom cls attrs (not the base python ones) -> if do not interfere with python cls parameters style
     attrs = inspect.getmembers(cls, lambda a: not (inspect.isroutine(a)))
+
     new_attrs = dict({tup[0]: tup[1] for tup in attrs}['__dict__'])
-    return {key: val for key, val in new_attrs.items() if not key.startswith('_')}
+    new_attrs = {key: val for key, val in new_attrs.items() if not key.startswith('_')}
+
+    for tup in attrs:
+        if not tup[0].startswith('_'):
+            new_attrs[tup[0]] = tup[1]
+
+    return new_attrs
