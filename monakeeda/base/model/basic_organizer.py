193147ea3633117d85c49e1a7df4b70a699ed37c
liel bondy <lielbondy@gmail.com>
2023-11-08 00:18:44 +0200

    [+] Added dependnecy considerations into component organizer implementation

--- monakeeda/base/model/basic_organizer.py
+++ monakeeda/base/model/basic_organizer.py
@@ -6,6 +6,7 @@ from typing import List, Dict, Type, Optional, Tuple
 from ..meta import ComponentOrganizer
 from ..component import Component, all_components
 from monakeeda.consts import NamespacesConsts, FieldConsts, ComponentConsts
+from monakeeda.utils import insert_if_does_not_exists
 
 
 class BaseComponentOrganizer(ComponentOrganizer):
@@ -40,17 +41,46 @@ class BaseComponentOrganizer(ComponentOrganizer):
 
         return None, -1
 
+    def _order_attr_scoped_components_for_instance_operation_per_field(self, field_key, monkey_cls, attrs_scoped_organized_components: Dict[Type[Component], List[Component]]) -> Tuple[List[Component], List[str]]:
+        """
+        Each field has in the struct namespace all the components that run logic on it.
+        These components already apper in the chain of responsibility order due to them being set up in the build logic which works according to it
+
+        Continuing with the sectioning logic explained in the main method, we take only the field's ordered components the current section (the current section is passed via attrs_scoped_organized_components).
+
+        Here we handle dependencies as well -> each field maps it dependencies in the build phase.
+        For each dependency we run recursively on it and so on and so on -> USES RECURSION
+
+        we return all the ordered components including the processed dependencies and all the processed field keys.
+        """
+
+        organized = []
+        processed_fields = []
+
+        dependencies = monkey_cls.struct[NamespacesConsts.FIELDS][field_key].setdefault(FieldConsts.DEPENDENCIES, [])
+        if dependencies:
+            for dependency_key in dependencies:
+                organized_dependencies, processed_dependencies = self._order_attr_scoped_components_for_instance_operation_per_field(dependency_key, monkey_cls, attrs_scoped_organized_components)
+                organized.extend(organized_dependencies)
+                processed_fields.extend(processed_dependencies)
+
+        attr_components = monkey_cls.struct[NamespacesConsts.FIELDS][field_key][FieldConsts.COMPONENTS]
+        attr_components = [component for component in attr_components if type(component) in attrs_scoped_organized_components.keys()]
+
+        organized.extend(attr_components)
+        processed_fields.extend(dependencies)
+        processed_fields.append(field_key)
+
+        return organized, processed_fields
+
     def _order_attr_scoped_components_for_instance_operation(self, monkey_cls, attrs_scoped_organized_components: Dict[Type[Component], List[Component]]) -> List[Component]:
         """
-        goes over the attrs scoped components received.
-            goes over each component instance in the different attrs scoped components types it works on currently
-                gets the field_key of each and finds all the attrs scoped components in the range that is currently looked on (According to the global scoped components range)
+        Goes over the attrs scoped components received - which are themselves ordered by the type based chained of responsibility.
+        For each component type it goes over the components, choosing a field key to start on loosely (dependencies are managed either way)
+        uses _order_attr_scoped_components_for_instance_operation_per_field to get the organized components of that field (including dependencies)
 
-        includes dependencies considerations -> e.g. CreateFrom appears prior to Validation but will look and process the dependencies prior
-
-        for these sub process we keep a list of listed_attrs -> meaning they were already processed in this current "batch".
+        for these sub process we keep a list of processed_attrs -> meaning they were already processed in this current "batch".
         """
-        # TODO: add dependency considerations
 
         organized = []
         processed_attrs = []
@@ -60,13 +90,9 @@ class BaseComponentOrganizer(ComponentOrganizer):
                 field_key = component._field_key
 
                 if field_key not in processed_attrs:
-                    attr_components = monkey_cls.struct[NamespacesConsts.FIELDS][field_key][FieldConsts.COMPONENTS]
-                    attr_components = [component for component in attr_components if type(component) in attrs_scoped_organized_components.keys()]
-                    # already set in organized order of chain of responsibility due to how the fact that the build stage is run in that order
-
-                    organized.extend(attr_components)
-
-                    processed_attrs.append(field_key)
+                    _organized, _processed_attrs = self._order_attr_scoped_components_for_instance_operation_per_field(field_key, monkey_cls, attrs_scoped_organized_components)
+                    organized.extend(_organized)
+                    processed_attrs.extend(_processed_attrs)
 
         # print(processed_attrs)
         return organized
