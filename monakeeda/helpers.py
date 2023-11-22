387b2250cabb9a61513567f05c272f1314aec7bd
liel bondy <lielbondy@gmail.com>
2023-11-22 17:37:58 +0200

    [+] Made a Helper defautldict for Exceptions structure -> this allows for a namespace for eeach exception group

--- monakeeda/helpers.py
+++ monakeeda/helpers.py
@@ -1,5 +1,6 @@
 import inspect
 from collections import defaultdict
+from functools import reduce
 
 
 class defaultdictvalue(defaultdict):
@@ -30,3 +31,17 @@ def get_cls_attrs(cls) -> dict:
             new_attrs[tup[0]] = tup[1]
 
     return new_attrs
+
+
+class ExceptionsDict(defaultdict):
+    def __init__(self):
+        super().__init__(lambda: [], {})
+
+    def __str__(self):
+        final_str = ""
+
+        for key, exceptions in self.items():
+            exceptions_str = str(reduce(lambda x1, x2: f"{x1} \n\t * {x2}", exceptions))
+            final_str += f" \n {key} -> \n\t * {exceptions_str}"
+
+        return final_str
