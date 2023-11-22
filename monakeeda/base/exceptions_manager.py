b731c3914ed5c6bd18d34b44bb6cf58dcc554566
liel bondy <lielbondy@gmail.com>
2023-11-22 23:41:35 +0200

    [+] Added is empty (__bool__) logic to ExceptionsDict and an if staement when parsint toString for the cases where trying to exactract exceptions of a scope but by doing so creating an empty list (defaultdict) - core logic that happens

--- monakeeda/base/exceptions_manager.py
+++ monakeeda/base/exceptions_manager.py
@@ -6,11 +6,19 @@ class ExceptionsDict(defaultdict):
     def __init__(self):
         super().__init__(lambda: [], {})
 
+    def __bool__(self):
+        for key, exceptions in self.items():
+            if exceptions:
+                return True
+
+        return False
+
     def __str__(self):
         final_str = ""
 
         for key, exceptions in self.items():
-            exceptions_str = str(reduce(lambda x1, x2: f"{x1} \n\t * {x2}", exceptions))
-            final_str += f" \n {key} -> \n\t * {exceptions_str}"
+            if exceptions:
+                exceptions_str = str(reduce(lambda x1, x2: f"{x1} \n\t * {x2}", exceptions))
+                final_str += f" \n {key} -> \n\t * {exceptions_str}"
 
         return final_str
