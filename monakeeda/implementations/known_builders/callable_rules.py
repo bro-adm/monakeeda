f302002a363656a8effbe6100798958aca3d2ea2
liel bondy <lielbondy@gmail.com>
2023-11-22 14:44:53 +0200

    [] Patched CallableParameterSignatureValidatorBuilder to work without inheritence using the patched MonkeyBuilder sub builders logic

--- monakeeda/implementations/known_builders/callable_rules.py
+++ monakeeda/implementations/known_builders/callable_rules.py
@@ -1,7 +1,7 @@
 import inspect
 from typing import Callable, List
 
-from monakeeda.base import Parameter
+from monakeeda.base import Parameter, MonkeyBuilder
 from .type_value_rules import BasicParameterValueTypeValidatorBuilder
 
 
@@ -15,19 +15,14 @@ class CallableOverTheAllowedAmountOfParametersException(Exception):
         return f"{self.parameter_key} method does not allow more than {self.amount_of_parameters_allowed} parameters -> amount asked for {self.amount_of_parameters_received}"
 
 
-class CallableParameterSignatureValidatorBuilder(BasicParameterValueTypeValidatorBuilder):
+class CallableParameterSignatureValidatorBuilder(MonkeyBuilder):
+    __builders__ = [BasicParameterValueTypeValidatorBuilder(Callable)]
+
     def __init__(self, amount_of_parameters_allowed: int):
-        super().__init__(Callable)
         self.amount_of_parameters_allowed = amount_of_parameters_allowed
 
     def _build(self, monkey_cls, bases, monkey_attrs, exceptions: List[Exception], main_builder: Parameter):
-        super_validation_result = super()._build(monkey_cls, bases, monkey_attrs, exceptions, main_builder)
+        callable_signature_parameters = inspect.signature(main_builder.param_val).parameters
 
-        if super_validation_result:
-            return super_validation_result
-
-        if not super_validation_result:
-            callable_signature_parameters = inspect.signature(main_builder.param_val).parameters
-
-            if len(callable_signature_parameters) != self.amount_of_parameters_allowed:
-                exceptions.append(CallableOverTheAllowedAmountOfParametersException(main_builder.__key__, self.amount_of_parameters_allowed, len(callable_signature_parameters)))
+        if len(callable_signature_parameters) != self.amount_of_parameters_allowed:
+            exceptions.append(CallableOverTheAllowedAmountOfParametersException(main_builder.__key__, self.amount_of_parameters_allowed, len(callable_signature_parameters)))
