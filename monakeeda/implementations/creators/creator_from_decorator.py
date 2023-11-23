00030e550abbb75586b5897f83927c0c384b12d0
liel bondy <lielbondy@gmail.com>
2023-11-23 15:50:59 +0200

    [] Updated CreateFrom decorator to sue known dependcies builder

--- monakeeda/implementations/creators/creator_from_decorator.py
+++ monakeeda/implementations/creators/creator_from_decorator.py
@@ -5,38 +5,32 @@ from monakeeda.consts import NamespacesConsts, FieldConsts
 from monakeeda.utils import get_wanted_params, wrap_in_list
 from .base_decorator import BaseCreatorDecorator
 from ..implemenations_base_operator_visitor import ImplementationsOperatorVisitor
+from ..known_builders import DependenciesBuilder
 from ..missing import ValidateMissingFieldsConfigParameter
 
 
 class CreateFrom(BaseCreatorDecorator):
     __prior_handler__ = ValidateMissingFieldsConfigParameter
+    __builders__ = [DependenciesBuilder()]
 
-    def __init__(self, field_key: str, from_keys: Union[list, str] = '*'):
+    def __init__(self, field_key: str, dependencies: Union[list, str] = '*'):
         super(CreateFrom, self).__init__(field_key)
-        self.from_keys = wrap_in_list(from_keys)
+        self.dependencies = wrap_in_list(dependencies)
 
     def _build(self, monkey_cls, bases, monkey_attrs, exceptions: ExceptionsDict, main_builder):
         super(CreateFrom, self)._build(monkey_cls, bases, monkey_attrs, exceptions, main_builder)
 
-        monkey_attrs[NamespacesConsts.STRUCT][NamespacesConsts.FIELDS][self._field_key][FieldConsts.DEPENDENCIES].extend(self.from_keys)
+        monkey_attrs[NamespacesConsts.STRUCT][NamespacesConsts.FIELDS][self.scope][FieldConsts.DEPENDENCIES].extend(self.dependencies)
 
-        # dependency key-values can be none schema parameters - therefore setdeafult is in use
-        if self.from_keys[0] == '*':
-            for key in monkey_attrs[NamespacesConsts.STRUCT][NamespacesConsts.FIELDS]:
-                if key != self._field_key:
-                    monkey_attrs[NamespacesConsts.STRUCT][NamespacesConsts.FIELDS][key][FieldConsts.DEPENDENTS].append(self._field_key)
-        else:
-            for key in self.from_keys:
-                monkey_attrs[NamespacesConsts.STRUCT][NamespacesConsts.FIELDS][key][FieldConsts.REQUIRED] = True
+        for key in self.dependencies:
+            monkey_attrs[NamespacesConsts.STRUCT][NamespacesConsts.FIELDS][key][FieldConsts.REQUIRED] = True
+            monkey_attrs[NamespacesConsts.STRUCT][NamespacesConsts.FIELDS][key][FieldConsts.DEPENDENTS].append(self.scope)
 
     def _handle_values(self, model_instance, values, stage, exceptions: ExceptionsDict):
         fields_info = getattr(model_instance, NamespacesConsts.STRUCT)[NamespacesConsts.FIELDS]
         configs = getattr(model_instance, NamespacesConsts.STRUCT)[NamespacesConsts.CONFIGS]
 
-        if self.from_keys[0] == '*':
-            wanted_val = self.func(model_instance, values, configs, fields_info)
-        else:
-            wanted_val = self.func(model_instance, configs, get_wanted_params(fields_info, self.from_keys), get_wanted_params(values, self.from_keys))
+        wanted_val = self.func(model_instance, configs, get_wanted_params(fields_info, self.dependencies), get_wanted_params(values, self.dependencies))
 
         values[self._field_key] = wanted_val
 
