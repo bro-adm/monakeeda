cc9ef9bacf3498a30b0573500f05e7c337d2736b
liel bondy <lielbondy@gmail.com>
2023-11-23 15:50:14 +0200

    []+]Added known depednciesbuilder implementation to be currently used by all decorators

--- monakeeda/implementations/known_builders/__init__.py
+++ monakeeda/implementations/known_builders/__init__.py
@@ -1,4 +1,5 @@
 from .core_annotations_extractor import CoreAnnotationsExtractor
+from .dependencies_builder import DependenciesBuilder
 from .fields_acknowledge_parameter_validator import AllFieldsAcknowledgeParameterValidator
 from .parameter_allowed_values_validator import ParameterAllowedValuesValidator
 from .parameter_callable_value_validator import ParameterCallableValueValidator
---
+++ monakeeda/implementations/known_builders/dependencies_builder.py
@@ -0,0 +1,30 @@
+from typing import List, Any
+
+from monakeeda.base import MonkeyBuilder, ExceptionsDict, Component
+from monakeeda.consts import NamespacesConsts
+
+
+class DependenciesKeysNotFieldsException(Exception):
+    def __init__(self, component: Component, provided_not_strs: List[Any]):
+        self.component_representor = component.representor
+        self.provided_not_strs = provided_not_strs
+
+    def __str__(self):
+        return f"{self.component_representor} component was provided with not field keys dependencies -> value {self.provided_not_strs}"
+
+
+class DependenciesBuilder(MonkeyBuilder):
+
+    def _build(self, monkey_cls, bases, monkey_attrs, exceptions: ExceptionsDict, main_builder: Component):
+
+        if main_builder.dependencies[0] == "*":
+            main_builder.dependencies = list(monkey_attrs[NamespacesConsts.STRUCT][NamespacesConsts.FIELDS].keys()).copy()
+            main_builder.dependencies.remove(main_builder.scope)
+        else:
+            not_strs = []
+            for key in main_builder.dependencies:
+                if not isinstance(key, str):
+                    not_strs.append(key)
+
+            if not_strs:
+                exceptions[main_builder.scope].append(ValueError(f"provided from keys not all str keys -> {not_strs}"))
