67d602973c681507e33e46c889a72126dc7e4f00
liel bondy <lielbondy@gmail.com>
2023-11-26 17:10:46 +0200

    [] Updated helper numeric constraints lambdas to include same constarint valdiation

--- monakeeda/implementations/numeric_constraints/helpers.py
+++ monakeeda/implementations/numeric_constraints/helpers.py
@@ -2,7 +2,11 @@ constraint_collisions_validation = {
     ('lt', 'gt'): lambda x, y: x - 1 > y,
     ('lt', 'lte'): lambda x, y: x - 1 == y,
     ('lt', 'gte'): lambda x, y: x > y,
+    ('lt', 'lt'): lambda x, y: x == y,
     ('gt', 'lte'): lambda x, y: x < y,
     ('gt', 'gte'): lambda x, y: x + 1 == y,
+    ('gt', 'gt'): lambda x, y: x == y,
     ('lte', 'gte'): lambda x, y: x >= y,
+    ('lte', 'lte'): lambda x, y: x == y,
+    ('gte', 'gte'): lambda x, y: x == y
 }
