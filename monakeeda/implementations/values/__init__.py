6238c04c512b22d27ed912ca57c23b2cb99bfc3d
liel bondy <lielbondy@gmail.com>
2023-11-28 00:57:22 +0200

    [+] Added file_input config parameter -> will later be managed via next feature of inputs manager

--- monakeeda/implementations/values/__init__.py
+++ monakeeda/implementations/values/__init__.py
@@ -1,3 +1,4 @@
 from .default_factory_field_parameter import DefaultFactoryFieldParameter
 from .default_field_parameter import DefaultFieldParameter
+from .file_input_config_parameter import FileInputConfigParameter
 from .value_field_parameter import ValueFieldParameter
---
+++ monakeeda/implementations/values/file_input_config_parameter.py
@@ -0,0 +1,57 @@
+import json
+from json import JSONDecodeError
+from typing import Any
+
+from pathvalidate import validate_filepath, ValidationError
+
+from monakeeda.base import ConfigParameter, Config, ExceptionsDict, OperatorVisitor, Component
+from monakeeda.utils import deep_update
+from ..abstract import AbstractParameter
+from ..known_builders import ParameterValueTypeValidator
+
+
+class FileInputException(Exception):
+    def __init__(self, component: Component, exception: ValidationError):
+        self.component_representor = component.representor
+        self.exception = exception
+
+    def __str__(self):
+        return f"{self.component_representor} error -> {str(self.exception)} -> exception_type = {type(self.exception)}"
+
+
+@Config.parameter
+class FileInputConfigParameter(ConfigParameter):
+    __key__ = "file_input"
+    __prior_handler__ = AbstractParameter
+    __builders__ = [ParameterValueTypeValidator(str)]
+
+    @property
+    def label(cls) -> str:
+        return 'value_provider'
+
+    @property
+    def scope(self) -> str:
+        return 'value_provider'
+
+    def _build(self, monkey_cls, bases, monkey_attrs, exceptions: ExceptionsDict, main_builder):
+        super()._build(monkey_cls, bases, monkey_attrs, exceptions, main_builder)
+
+        try:
+            validate_filepath(self.param_val)
+            print(validate_filepath(self.param_val))
+        except ValidationError as e:
+            exceptions[self.scope].append(FileInputException(self, e))
+
+    def _handle_values(self, model_instance, values, stage, exceptions: ExceptionsDict):
+        try:
+            with open(self.param_val, 'r') as file:
+                file_content = file.read()
+                file_values = json.loads(file_content)
+
+            deep_update(values, file_values)
+
+        except (FileNotFoundError, JSONDecodeError) as e:
+            exceptions[self.scope].append(FileInputException(self, e))
+
+    def accept_operator(self, operator_visitor: OperatorVisitor, context: Any):
+        pass
