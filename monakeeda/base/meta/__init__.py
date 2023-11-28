e9d49c157e885a937a1f86a3e13c448ab0c4b71c
liel bondy <lielbondy@gmail.com>
2023-11-28 21:23:33 +0200

    [+] Added delayed build to metaclass logic -> yet to add intialization factor

--- monakeeda/base/meta/__init__.py
+++ monakeeda/base/meta/__init__.py
@@ -1,5 +1,5 @@
 from .component_manager import ComponentManager, ConfigurableComponentManager
 from .components_organizer import ComponentsOrganizer
 from .errors import MonkeyBuildException
-from .mokey_meta import MonkeyMeta
+from .mokey_meta import MonkeyMeta, delayed_monkeys
 from .scopes_manager import ScopesManager
--- monakeeda/base/meta/mokey_meta.py
+++ monakeeda/base/meta/mokey_meta.py
@@ -8,6 +8,8 @@ from .helpers import handle_class_inputs
 from ..scope import ScopeDict
 from ..exceptions_manager import ExceptionsDict
 
+delayed_monkeys = []
+
 
 class MonkeyMeta(ABCMeta):
     """
@@ -35,37 +37,42 @@ class MonkeyMeta(ABCMeta):
         cls = super(MonkeyMeta, mcs).__new__(mcs, name, bases, attrs)
         return cls
 
-    def __init__(cls, name, bases, attrs, component_managers=None, scopes_manager=None, component_organizer=None, operators_visitors=None):
-        handle_class_inputs(cls, bases, component_managers=component_managers, scopes_manager=scopes_manager, component_organizer=component_organizer, operators_visitors=operators_visitors)
+    def __init__(cls, name, bases, attrs, component_managers=None, scopes_manager=None, component_organizer=None, operators_visitors=None, delay=False):
+        if not delay:
+            handle_class_inputs(cls, bases, component_managers=component_managers, scopes_manager=scopes_manager, component_organizer=component_organizer, operators_visitors=operators_visitors)
 
-        super(MonkeyMeta, cls).__init__(name, bases, attrs)
+            super(MonkeyMeta, cls).__init__(name, bases, attrs)
 
-        monkey_bases = [base for base in bases if type(base) == MonkeyMeta]
-        logger.info(f"Monkey Bases = {monkey_bases}", extra={STAGE: "META SETUP", MONKEY: name})
+            monkey_bases = [base for base in bases if type(base) == MonkeyMeta]
+            logger.info(f"Monkey Bases = {monkey_bases}", extra={STAGE: "META SETUP", MONKEY: name})
 
-        for component_manager in cls.__component_managers__:
-            component_manager.build(cls, monkey_bases, attrs, {})
+            for component_manager in cls.__component_managers__:
+                component_manager.build(cls, monkey_bases, attrs, {})
 
-        model_components = attrs[NamespacesConsts.COMPONENTS]
-        logger.info(f"Post Component Managers All Model Components = {model_components}", extra={STAGE: "Component Info", MONKEY: name})
-        cls.__type_organized_components__ = cls.__component_organizer__.order_by_chain_of_responsibility(model_components)
-        logger.info(f"Post Component Managers Organized Model Components = {cls.__type_organized_components__}", extra={STAGE: "Component Info", MONKEY: name})
+            model_components = attrs[NamespacesConsts.COMPONENTS]
+            logger.info(f"Post Component Managers All Model Components = {model_components}", extra={STAGE: "Component Info", MONKEY: name})
+            cls.__type_organized_components__ = cls.__component_organizer__.order_by_chain_of_responsibility(model_components)
+            logger.info(f"Post Component Managers Organized Model Components = {cls.__type_organized_components__}", extra={STAGE: "Component Info", MONKEY: name})
 
-        build_exceptions = ExceptionsDict()
-        for component_type, components in cls.__type_organized_components__.items():
-            for component in components:
-                component.build(cls, bases, attrs, build_exceptions)
+            build_exceptions = ExceptionsDict()
+            for component_type, components in cls.__type_organized_components__.items():
+                for component in components:
+                    component.build(cls, bases, attrs, build_exceptions)
 
-        cls.__scopes_manager__.build(cls, bases, attrs, build_exceptions)
+            cls.__scopes_manager__.build(cls, bases, attrs, build_exceptions)
 
-        if build_exceptions:
-            raise MonkeyBuildException(name, build_exceptions)
+            if build_exceptions:
+                raise MonkeyBuildException(name, build_exceptions)
 
-        logger.info(f"Post Components Build Organized Model Components = {cls.__type_organized_components__}", extra={STAGE: "Component Info", MONKEY: name})
-        logger.info(f"Scopes = {attrs[NamespacesConsts.SCOPES]}", extra={STAGE: "Component Info", MONKEY: name})
+            logger.info(f"Post Components Build Organized Model Components = {cls.__type_organized_components__}", extra={STAGE: "Component Info", MONKEY: name})
+            logger.info(f"Scopes = {attrs[NamespacesConsts.SCOPES]}", extra={STAGE: "Component Info", MONKEY: name})
 
-        cls.__run_organized_components__ = cls.__component_organizer__.order_for_instance_operation(cls, cls.__type_organized_components__)
-        logger.info(f"Model Components Run Order:", extra={STAGE: "Component Info", MONKEY: name})
+            cls.__run_organized_components__ = cls.__component_organizer__.order_for_instance_operation(cls, cls.__type_organized_components__)
+            logger.info(f"Model Components Run Order:", extra={STAGE: "Component Info", MONKEY: name})
 
-        for component in cls.__run_organized_components__:
-            logger.info(f"\t{component} -> {component.scope}", extra={STAGE: "Component Info", MONKEY: name})
+            for component in cls.__run_organized_components__:
+                logger.info(f"\t{component} -> {component.scope}", extra={STAGE: "Component Info", MONKEY: name})
+
+        else:
+            cls._init_args = ((name, bases, attrs), {"component_managers": component_managers, "scopes_manager": scopes_manager, "component_organizer": component_organizer, "operators_visitors": operators_visitors})
+            delayed_monkeys.append(cls)
