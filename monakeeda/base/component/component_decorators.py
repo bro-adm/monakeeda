be0b57a50aa3768c8b20bce390cd2f0fc5ce64a5
liel bondy <lielbondy@gmail.com>
2023-12-09 17:06:59 +0200

    [+] Added conceptual support for nested decoration in component decortaor class

--- monakeeda/base/component/component_decorators.py
+++ monakeeda/base/component/component_decorators.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import List
+from typing import List, Union
 
 from .component import Component
 from ..exceptions_manager import ExceptionsDict
@@ -8,10 +8,17 @@ from ..interfaces import ValuesHandler
 
 class ComponentDecorator(ValuesHandler, ABC):
     def __init__(self):
-        self.component: Component = None
+        self.component: Union[Component, 'ComponentDecorator'] = None
 
     def _extract_relevant_exceptions(self, exceptions: ExceptionsDict) -> List[Exception]:
         return []
+
+    @property
+    def actual_component(self) -> Component:
+        if isinstance(self.component, ComponentDecorator):
+            return self.component.actual_component
+
+        return self.component
 
     @abstractmethod
     def reset(self):
